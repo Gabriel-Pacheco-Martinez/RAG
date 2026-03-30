0d261c81b7523d9776af95a806b6c5343acad42b
Gabriel-Pacheco-Martinez <gabrielpachecomartinez2000@gmail.com>
2026-03-30 12:23:35 -0700

    Changed microservices configuration

--- rerank_server/config/settings.py
+++ rerank_server/config/settings.py
@@ -4,18 +4,17 @@ from qdrant_client import AsyncQdrantClient
 # HuggingFace
 from sentence_transformers import CrossEncoder
 
-# =====
-# Qdrant client
-ASYNC_QDRANT_CLIENT = AsyncQdrantClient(
-    url="http://qdrant:6333",  # Docker exposed port
-    # url="http://localhost:6333",  # Localhost port
-)
-TOP_K_DENSE = 8
-TOP_K_SPARSE = 8
-LIMIT_N_HYBRID_CAPS = 3
-LIMIT_N_HYBRID_TEXT = 4
+# Configuration
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-# =====
-# Embeddings
-RERANKER_MODEL = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
-# RERANKER_MODEL = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-2-v2")
+class Settings(BaseSettings):
+    # Load variables
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+
+    # RERANKER MODEL
+    RERANKER_MODEL_NAME: str = "cross-encoder/ms-marco-MiniLM-L-6-v2"
+
+
+settings = Settings()
+
+RERANKER_MODEL = CrossEncoder(settings.RERANKER_MODEL_NAME)
