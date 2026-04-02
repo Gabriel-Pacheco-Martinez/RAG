81fa6b1d09f93fbfcfbac7a1c1b8daf7bae9bad3
Gabriel-Pacheco-Martinez <gabrielpachecomartinez2000@gmail.com>
2026-04-02 12:58:40 -0700

    change number of workers

--- api_server/app/endpoint.py
+++ api_server/app/endpoint.py
@@ -161,5 +161,5 @@ def start_server(host: str = "0.0.0.0", port: int = 8000):
         reload=False,
         log_level="info",
         log_config="config/logconfig.json",
-        workers=2
+        workers=1
     )
---
+++ build.sh
@@ -0,0 +1,4 @@
+# Build api_server image
+docker build -f docker-compose-monolith.yml -t api_server 
+
+# Build 
